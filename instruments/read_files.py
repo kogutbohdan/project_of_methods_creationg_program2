28ca32e2bd15001b652410f218db6310316b2c40
kohutbohdan <bohdan.v.kohut@ukd.edu.ua>
2026-03-07 16:48:14 +0200

    code with logic

---
+++ instruments/read_files.py
@@ -0,0 +1,41 @@
+import pdfplumber
+#from docx import Document
+from bs4 import BeautifulSoup
+from io import BytesIO
+from sentence_transformers import SentenceTransformer
+
+def read_pdf(file=None):
+    with pdfplumber.open(BytesIO(file.content)) as f:
+        text="" 
+        for page in f.pages:
+            text+=page.extract_text()
+    return text
+
+def read_docs(file=None):
+    print("DOCS")
+
+def read_html(file=None):
+    soup=BeautifulSoup(file.content,"html.parser")
+    for tag in soup.find_all(True):
+        tag.unwrap()
+    return soup.get_text(separator=" ")
+
+
+class FileReader:
+    __methods_for_read_file={
+        "application/pdf":read_pdf,
+        "application/msword":read_docs,
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document":read_docs,
+        "text/html; charset=utf-8":read_html,
+        "text/html":read_html
+    }
+
+    def read(self,content_type,file=None):
+        print("rabar"+content_type+"rabar")
+        if content_type in self.__methods_for_read_file.keys():
+            return self.__methods_for_read_file[content_type](file)
+        print("Невідомий файл")
+    
+    def get_embedding(self,content_type,file=None):
+        sentens_transformer=SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+        return sentens_transformer.encode([self.read(content_type=content_type,file=file)])
--- main.py
+++ main.py
@@ -1,6 +1,46 @@
 from fastapi import FastAPI
 from sentence_transformers import SentenceTransformer
+import requests as req
+from instruments.read_files import FileReader
+from pymilvus import (
+    connections,
+    db,
+    FieldSchema,
+    CollectionSchema,
+    DataType,
+    Collection
+)
 
+connections.connect(
+    alias="default",
+    host="localhost",
+    port="5000"
+)
+
+db_name="files"
+if db_name not in db.list_database():
+    db.create_database(db_name=db_name)
+
+db.using_database(db_name=db_name)
+
+schema=CollectionSchema([
+    FieldSchema(name="id",dtype=DataType.INT64,is_primary=True,auto_id=True),
+    FieldSchema(name="vector",dtype=DataType.FLOAT_VECTOR,dim=384),
+    FieldSchema(name="url",dtype=DataType.VARCHAR,max_length=1000),
+    FieldSchema(name="name",dtype=DataType.VARCHAR,max_length=100)
+])
+collection = Collection(name="multilingual_vectors", schema=schema)
+
+collection.create_index({
+    "vector",
+    {
+        "metric_type":"IP",
+        "index_type":"HNSW",
+        "params":{"M":8,"efConstruction":64}
+    }
+})
+
+collection.load()
 
 app = FastAPI()
 
@@ -11,5 +51,19 @@ def root():
 @app.post("/query")
 def complete_query(query:str):
     sentens_transformer=SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
-    print(sentens_transformer.encode([query]))
+    query_embedding=sentens_transformer.encode([query])
     return {"ok":True}
+
+@app.post("/file")
+def add_file(path_file:str):
+    try:
+        file=req.get(path_file,headers={
+            "User-Agent": "Mozilla/5.0"
+        })
+        content_type = file.headers.get("Content-Type")
+        reader=FileReader()
+        collection.insert([reader.get_embedding(content_type=content_type,file=file),path_file,reader.read(content_type=content_type,file=file).split(" ")[0:10].join(" ")])
+        collection.load()
+        return {"ok":True}
+    except:
+        return {"ok":False}
